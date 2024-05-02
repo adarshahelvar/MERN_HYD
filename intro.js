19178b29965ce809145cfce1ac0957c97af03bbc
Dineshkuna <146323388+Dineshkuna@users.noreply.github.com>
2024-05-02 13:07:27 +0530

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,16 @@
+
+// Browser	Name of Javascript Engine
+// Google Chrome	- V8
+// Edge (Internet Explorer) -	Chakra
+// Mozilla Firefox	 - Spider Monkey
+// Safari -	Javascript Core Webkit
+
+
+
+console.log("hi")
+
+
+const fs = require('fs')
+
+fs.writeFileSync ("read.txt", "Hello")
+
