69d8ee97a4fa528324bf896cd4191c3a3d5ce3c8
adarshahelvar <adarshaadi1997@gmail.com>
2024-05-09 10:06:25 +0530

    Routing and parsing in ExpressJS

--- Server/app.js
+++ Server/app.js
@@ -1,15 +1,26 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("Hello from ExpressJS");
-  next();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/add-product", (req, res, next) => {
+  // console.log("Second middleware function");
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button></form>"
+  );
 });
 
-app.use((req, res, next) => {
-  console.log("Second middleware function");
-  res.send("<h1>Hello from Express Server</h1>");
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  console.log("Hello from ExpressJS");
+  res.send("<h1>Hello from Express</h1>");
+  // next();
 });
 
 app.listen(3000);
