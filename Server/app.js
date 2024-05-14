87f770aa4f7c1091c4f2a209c838e892b0cdb98c
adarshahelvar <adarshaadi1997@gmail.com>
2024-05-14 09:55:23 +0530

    File serving

--- Server/app.js
+++ Server/app.js
@@ -2,10 +2,14 @@ const express = require("express");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Static css file serving
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
