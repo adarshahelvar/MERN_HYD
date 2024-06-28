6de29166783dee7413d77e845fb86020d98f397c
adarshahelvar <adarshaadi1997@gmail.com>
2024-06-28 13:23:49 +0530

    Get all blogs

--- blog/controller/blogController.js
+++ blog/controller/blogController.js
@@ -32,3 +32,56 @@ export const creteBlog = async (req, res, next) => {
       .json({ success: false, message: "Something went wrong...!" });
   }
 };
+
+export const getAllBlogs = async (req, res) => {
+  try {
+    const blog = await Blog.find();
+    res.status(200).json({ success: true, message: "Blogs found", data: blog });
+  } catch (error) {
+    res.status(404).json({ success: false, message: "Blogs not found" });
+  }
+};
+
+export const getSingleBlog = async (req, res) => {
+  const blogId = req.params.id;
+
+  try {
+    const blog = await Blog.findById(blogId);
+
+    if (!blog) {
+      res.status(404).json({ success: false, message: "Blog not found" });
+    }
+
+    res.status(200).json({ success: true, message: "Blogs found", data: blog });
+  } catch (error) {
+    res.status(404).json({ success: false, message: "Blog not found" });
+  }
+};
+
+export const deleteBlog = async (req, res) => {
+  const blogId = req.params.id;
+  const userId = req.userId;
+  try {
+    const blog = await Blog.findById(blogId);
+    if (!blog) {
+      return res
+        .status(500)
+        .json({ success: false, message: "No blog found with this ID" });
+    }
+
+    // console.log("userId", userId);
+    // console.log(blog.user.id);
+    if (blog.user.id.toString() !== userId.toString()) {
+      res.status(403).json({
+        success: false,
+        message: "Unauthorized: You can't delete this blog.",
+      });
+    }
+
+    await Blog.findByIdAndDelete(blogId);
+    res.status(200).json({ success: true, message: "Blog Deleted" });
+  } catch (error) {
+    // console.log(error);
+    return res.status(500).json({ success: false, message: "Server error" });
+  }
+};
--- blog/routes/blogRoute.js
+++ blog/routes/blogRoute.js
@@ -1,7 +1,15 @@
 import express from "express";
-import { creteBlog } from "../controller/blogController.js";
+import {
+  creteBlog,
+  deleteBlog,
+  getAllBlogs,
+  getSingleBlog,
+} from "../controller/blogController.js";
 import { authenticate, restrict } from "../auth/verifyToken.js";
 
 const router = express.Router();
 router.post("/createblog", authenticate, creteBlog);
+router.get("/getallblog", getAllBlogs);
+router.get("/getsingleblog/:id", getSingleBlog);
+router.delete("/deleteblog/:id", authenticate, deleteBlog);
 export default router;
