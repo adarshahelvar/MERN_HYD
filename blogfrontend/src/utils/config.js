5f2096b4ce944ef211c39dbb51f9bc844c8ef6e2
adarshahelvar <adarshaadi1997@gmail.com>
2024-07-12 12:46:28 +0530

    Live backend URL update

--- blogfrontend/src/utils/config.js
+++ blogfrontend/src/utils/config.js
@@ -1,4 +1,5 @@
-export const BASE_URL = "http://localhost:8000";
+// export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "https://mern-hyd.onrender.com";
 
 export const token = localStorage.getItem("token");
 
